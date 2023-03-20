fefb3a31cbc393e9aafaafb2290302726a1aeee0
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-03-20 21:01:34 +0530

    done

---
+++ day1/tilting_nav.js
@@ -0,0 +1,19 @@
+const open = document.querySelector("#open")
+const close = document.querySelector("#close")
+const container = document.querySelector('.container')
+
+
+open.addEventListener("click",()=>{
+    myFun()
+})
+
+close.addEventListener("click",()=>{
+    myFun2()
+})
+const myFun=()=>{
+    container.classList.add('show-nav')
+}
+
+const myFun2=()=>{
+    container.classList.remove('show-nav')
+}
